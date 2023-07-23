061db6d511142d89c06bee22ca3c7d4cb45794ce
poxyinaproblemi <camry4793cp@gmail.com>
2023-07-23 16:56:24 +0300

    Добавлен код для подсчета суммы чисел от 1 до N

---
+++ Practice For.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main() {
+
+    setlocale(LC_ALL, "russian");
+    int N;
+    std::cout << "Введите число N: ";
+    std::cin >> N;
+
+    int sum = 0;
+    for (int i = 1; i <= N; ++i) {
+        sum += i;
+    }
+
+    std::cout << "Сумма чисел от 1 до " << N << " = " << sum << std::endl;
+    return 0;
+}
