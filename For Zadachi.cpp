7d3ec0d53ad5dc9e59d9f0695afbd1bb8b46b154
poxyinaproblemi <camry4793cp@gmail.com>
2023-07-23 17:12:49 +0300

    Добавлен файл For Zadachi.cpp с задачей для вычисления факториала числа N

---
+++ For Zadachi.cpp
@@ -0,0 +1,21 @@
+﻿/* Факториал числа 
+* Запроси у пользователя число N и вычисли его факториал (произведение всех положительных целых чисел от 1 до N).
+* 
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL,"russian");
+    int N;
+    std::cout << "Введите число N: ";
+    std::cin >> N;
+
+    int factorial = 1;
+    for (int i = 1; i <= N; ++i) {
+        factorial *= i;
+    }
+
+    std::cout << N << "! = " << factorial << std::endl;
+    return 0;
+}
+
+*/
